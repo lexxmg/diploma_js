91e4ec37b2a20371ba04a20c00447f9567f8f0e0
mac-mini <lexx.mg@gmail.com>
2021-03-13 21:21:52 +0300

    sync

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 
 import './App.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { unsplashApi } from './api/api';
 
 function App() {
+  const [ images, setImages ] = useState(null);
+
   useEffect(() => {
-    unsplashApi.getPhotos(1, 3).then(photos => {
+    unsplashApi.getPhotos(2, 9).then(photos => {
+      setImages(photos);
       console.log(photos);
+      console.log(photos[0].urls.small);
     })
   }, []);
 
   return (
-    <div className="App">
-
+    <div className="App fixed-container">
+      <div className="App__inner">
+        {
+          images && images.map(obj => {
+            return (
+              <img className="App__img" key={obj.id} src={obj.urls.small} alt={obj.alt_description} />
+            )
+          })
+        }
+      </div>
     </div>
   );
 }
