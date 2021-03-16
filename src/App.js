6768960b83be0ab47d4b21e153c9952fe61960b8
mac-book <lexx.mg@gmail.com>
2021-03-16 19:24:52 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 
 import './App.css';
 import { useEffect, useState } from 'react';
-import { unsplashApi } from './api/api';
+import { api } from './api/api';
 import Masonry, {ResponsiveMasonry} from 'react-responsive-masonry';
 
 function App() {
-  const [ images, setImages ] = useState(null);
+  const [ currentPages, setCurrentPages ] = useState(1);
+  const [ images, setImages ] = useState([]);
 
   useEffect(() => {
-    unsplashApi.getPhotos(2, 9).then(photos => {
-      setImages(photos);
-      console.log(photos);
-      console.log(photos[0].urls.small);
+    api.getPhotos(currentPages, 2).then(photos => {
+      setImages([...images, ...photos]);
+      // console.log(photos);
+      // console.log(photos[0].urls.small);
     })
-  }, []);
+  }, [currentPages]);
 
   return (
     <div className="App fixed-container">
       <div className="App__inner">
-      { images &&
+
+      <button className="App__btn" onClick={() => api.auth('U9Divbch2kk')}>auth</button>
+      <button onClick={() => {
+        setCurrentPages(currentPages + 1);
+      }}>ещё</button>
+      
       <ResponsiveMasonry
         columnsCountBreakPoints={{350: 1, 750: 2, 900: 3}}
       >
@@ -26,13 +32,20 @@ function App() {
             {
               images.map(obj => {
                 return (
-                  <img className="App__img" key={obj.id} src={obj.urls.small} alt={obj.alt_description} />
+                  <div className="" key={obj.id}>
+                    <img className="App__img" src={obj.urls.small} alt={obj.alt_description} />
+                    <span style={{marginRight: '20px'}}>{obj.id}</span>
+
+                    <button className="App__btn" onClick={() => api.like(obj.id)}>like</button>
+                    <button className="App__btn" onClick={() => api.unLike(obj.id)}>unLike</button>
+
+                    <span style={{marginLeft: '20px'}}>{obj.likes}</span>
+                  </div>
                 )
               })
             }
           </Masonry>
         </ResponsiveMasonry>
-      }
       </div>
     </div>
   );
--- src/api/api.js
+++ src/api/api.js
@@ -1,33 +1,23 @@
 
-import { createApi } from 'unsplash-js';
+import * as axios from 'axios';
 
-const unsplash = createApi({
-  accessKey: '_i66pu-AAF7Fk6drWH2jys78579pswAF2DW8Q__DSeQ'
-  //...other fetch options
+const instance = axios.create({
+  baseURL: 'https://api.unsplash.com/',
+  headers: {'Authorization': 'Client-ID _i66pu-AAF7Fk6drWH2jys78579pswAF2DW8Q__DSeQ'}
 });
 
-export const unsplashApi = {
-  getUserPhotos() {
-    return unsplash.users.getPhotos({ username: 'foo' }).then(result => {
-      if (result.errors) {
-        // handle error here
-        console.log('error occurred: ', result.errors[0]);
-      } else {
-        const feed = result.response;
-
-        // extract total and results array from response
-        const { total, results } = feed;
-
-        // handle success here
-        console.log(`received ${results.length} photos out of ${total}`);
-        console.log('first photo: ', results[0]);
-      }
-    });
+export const api = {
+  auth() {
+    return instance.get('oauth').then(res => console.log(res))
   },
   getPhotos(page, perPage) {
-    return unsplash.photos.list({ page, perPage }).then(res => {
-      console.log(res);
-      return res.response.results
-    })
+    return instance.get(`photos?page=${page}&per_page=${perPage}`)
+      .then(res => res.data)
+  },
+  like(id){
+    return instance.post(`photos/${id}/like`).then(res => console.log(res));
+  },
+  unLike(id){
+    return instance.delete(`photos/${id}/like`).then(res => console.log(res));
   }
 }
