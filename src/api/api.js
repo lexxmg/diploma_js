25180cc297796ab88ebf5e89dd8db254cb26c71d
mac-book <lexx.mg@gmail.com>
2021-03-17 19:56:05 +0300

    fix

--- src/api/api.js
+++ src/api/api.js
@@ -2,10 +2,14 @@
 import * as axios from 'axios';
 import url from 'url';
 
+const CLIENT_ID = '_i66pu-AAF7Fk6drWH2jys78579pswAF2DW8Q__DSeQ',
+      CLIENT_SECRET = '_Cp8wCr4mkPd0YqSQiDJAPLUtwi_wORX_gixD3WRCUU',
+      REDIRECR_URL = 'http://localhost:3000'
+
 const code = url.parse(document.URL, 'query=string');
 console.log(code.query.code);
 
-let authorization = 'Client-ID _i66pu-AAF7Fk6drWH2jys78579pswAF2DW8Q__DSeQ';
+let authorization = `Client-ID ${CLIENT_ID}`;
 
 let instance = axios.create({
   baseURL: 'https://api.unsplash.com/',
@@ -15,14 +19,14 @@ let instance = axios.create({
   }
 });
 
-export const authUrl = 'https://unsplash.com/oauth/authorize?client_id=_i66pu-AAF7Fk6drWH2jys78579pswAF2DW8Q__DSeQ&redirect_uri=http://localhost:3000&response_type=code&scope=public+write_likes';
+export const authUrl = `https://unsplash.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECR_URL}&response_type=code&scope=public+write_likes`;
 
 export const api = {
   auth() {
     axios.post('https://unsplash.com/oauth/token', {
-      client_id: '_i66pu-AAF7Fk6drWH2jys78579pswAF2DW8Q__DSeQ',
-      client_secret: '_Cp8wCr4mkPd0YqSQiDJAPLUtwi_wORX_gixD3WRCUU',
-      redirect_uri: 'http://localhost:3000',
+      client_id: CLIENT_ID,
+      client_secret: CLIENT_SECRET,
+      redirect_uri: REDIRECR_URL,
       code: code.query.code,
       grant_type: 'authorization_code'
     }).then(res => {
