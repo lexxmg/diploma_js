c49281873e2e7b9e0d476b7ef8cb5ecfe9ee6043
mac-book <lexx.mg@gmail.com>
2021-03-25 20:55:31 +0300

    add background loading

--- src/components/PhotoFull/PhotoFull/PhotoFull.jsx
+++ src/components/PhotoFull/PhotoFull/PhotoFull.jsx
@@ -3,11 +3,11 @@ import './photo-full.css';
 
 const PhotoFull = (props) => {
   return (
-    <div>
+    <div className="photo-full__container">
       <div className="photo-full__img-container">
         {
           props.fullPhoto &&
-            <img
+            <img className="photo-full__img"
               src={props.fullPhoto.urls.full}
               alt={props.fullPhoto.alt_description}
             />
