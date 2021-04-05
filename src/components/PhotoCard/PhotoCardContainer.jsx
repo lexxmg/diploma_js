c3d9a7efca0f0c984db96825484c5beb3f6bc64b
lexx mg <lexxmg@iMac-lexx-3.local>
2021-04-05 13:39:49 +0300

    sync

--- src/components/PhotoCard/PhotoCardContainer.jsx
+++ src/components/PhotoCard/PhotoCardContainer.jsx
@@ -8,85 +8,52 @@ import PhotoCatd from './PhotoCard/PhotoCard';
 import Preloader from '../Common/Preloader/Preloader.jsx';
 
 const PhotoCardContainer = (props) => {
-  const [ load, setLoad ] = useState(true);
-  const [ scrollBarWidth, setScrollBarWidth ] = useState(0);
+  const observer = new IntersectionObserver(observerHandle, { threshold: 1 });
 
   useEffect(() => {
-    const photoEnd = document.querySelectorAll('.div-end');
-    if (photoEnd) {
-    const observer = new IntersectionObserver(entries => {
-      entries.forEach((item, i) => {
-        if (item.intersectionRatio <= 0) return;
-        //console.log(item);
-        console.log('load next');
-        addPhoto();
-
-        observer.unobserve(photoEnd[i]);
-      });
-    }, { threshold: 1 });
-
-
-      photoEnd.forEach((item, i) => {
-        observer.observe(photoEnd[i]);
-      });
-
-      return () => {
-        photoEnd.forEach((item, i) => {
-          observer.unobserve(photoEnd[i]);
-        });
-      }
+    if (props.photos.length === 0 && !props.loading) {
+      addPhoto();
     }
   });
 
-  // useEffect(() => {
-  //   const clientWidth = document.documentElement.clientWidth;
-  //
-  //   document.documentElement.style.overflow = 'hidden';
-  //   setScrollBarWidth(document.documentElement.clientWidth - clientWidth);
-  //   document.documentElement.style.overflow = '';
-  //   console.log(scrollBarWidth);
-  // }, [scrollBarWidth]);
+  function observerHandle(entries, observer) {
+    const photoEnd = document.querySelectorAll('.div-end');
 
-  const addPhoto = () => {
+    entries.forEach((item, i) => {
+      if (item.intersectionRatio <= 0) return;
+      //console.log(item);
+      console.log('load next');
+      addPhoto();
+
+      for (let i = 0; i < photoEnd.length; i++) {
+        observer.unobserve(photoEnd[i]);
+      }
+    });
+  }
+
+  useEffect(() => {
+    const photoEnd = document.querySelectorAll('.div-end');
+      if (photoEnd) {
+
+        photoEnd.forEach((item, i) => {
+          observer.observe(photoEnd[i]);
+        });
+
+        return () => {
+          photoEnd.forEach((item, i) => {
+            observer.unobserve(photoEnd[i]);
+          });
+        }
+      }
+  });
+
+  function addPhoto() {
     props.getPhoto(props.currentPage, 10)
       .then(() => {
         props.setCurrentPage(props.currentPage + 1);
         //setLoad(true);
       });
   }
-
-  const scrollEnd = () => {
-    const position = (
-      document.body.clientHeight - document.documentElement.clientHeight - window.pageYOffset
-    );
-
-    //console.log(position);
-
-    if (position <= 300 && position > 0 && load ) {
-      addPhoto();
-      setLoad(false);
-    }
-  }
-
-  useEffect(() => {
-    // if (props.loading) {
-    //   document.documentElement.style.overflow = 'hidden';
-    //   document.documentElement.style.paddingRight = scrollBarWidth + 'px';
-    // } else {
-    //   document.documentElement.style.overflow = '';
-    //   document.documentElement.style.paddingRight = '';
-    // }
-
-    if (props.photos.length === 0 && !props.loading) {
-      addPhoto();
-    }
-    //
-    // document.addEventListener('scroll', scrollEnd);
-    //
-    // return () => {
-    //   document.removeEventListener('scroll', scrollEnd);
-    // }
-  });
 
   return (
     <div className="photo-card-container fixed-container">
