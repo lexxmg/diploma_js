288abe2bb09aed8ccb1cb5eff0393bd987185e51
lexx mg <lexxmg@iMac-lexx-3.local>
2021-04-16 12:35:19 +0300

    sync

--- src/components/Auth/Auth/Auth.jsx
+++ src/components/Auth/Auth/Auth.jsx
@@ -8,7 +8,7 @@ import { compose } from 'redux';
 import { login } from '../../../redux/auth.js'
 
 const Auth = ({ isAuth, login }) => {
-  const currentFullPhoto = window.localStorage.getItem('currentFullPhoto');
+  const currentFullPhotoId = window.localStorage.getItem('currentFullPhotoId');
 
   useEffect(() => {
     //console.log(props);
@@ -19,7 +19,7 @@ const Auth = ({ isAuth, login }) => {
   return (
     <div className="auth-container">
       <div className="auth-container__plaseholder"></div>
-      { isAuth && <Redirect to="/" />}
+      { isAuth && <Redirect to={currentFullPhotoId ? "/full/" + currentFullPhotoId : "/"} />}
     </div>
   )
 }
