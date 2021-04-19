912b3f51cfb74a8f15da57ef0dfcfae32ec29a12
lexx mg <lexxmg@iMac-lexx-3.local>
2021-04-19 13:11:55 +0300

    edit poto card styles

--- src/components/PhotoCard/PhotoCard/PhotoCard.jsx
+++ src/components/PhotoCard/PhotoCard/PhotoCard.jsx
@@ -8,18 +8,22 @@ const PhotoCatd = (props) => {
   const [ descriptionShow, setDescriptionShow ] = useState(false);
 
   //console.log(props.photo);
-
+  //cn('photo-card', {darkened: descriptionShow})
   return (
-    <div className={cn('photo-card', {darkened: descriptionShow})}
-      onMouseOver={() => setDescriptionShow(true)}
-      onMouseOut={() => setDescriptionShow(false)}
+    <div
+      className="photo-card"
+      onMouseEnter={() => setDescriptionShow(true)}
+      onMouseLeave={() => setDescriptionShow(false)}
     >
-
-      <img
-        className="photo-card__img"
-        src={props.photo.urls.small}
-        alt={props.photo.alt_description}
-      />
+      <NavLink to={"/full/" + props.photo.id} className="photo-card__nav-link">
+        <div className={cn('photo-card__container', {darkened: descriptionShow})}>
+          <img
+            className="photo-card__img"
+            src={props.photo.urls.small}
+            alt={props.photo.alt_description}
+          />
+        </div>
+      </NavLink>
 
       {descriptionShow && <PhotoDescriptions {...props} />}
     </div>
@@ -29,8 +33,6 @@ const PhotoCatd = (props) => {
 function PhotoDescriptions(props) {
   return (
     <div className="photo-card__descriptions photo-card-descriptions">
-      <NavLink to={"/full/" + props.photo.id}>+</NavLink>
-
       <div className="photo-card-descriptions__container">
         <span className="photo-card-descriptions__like-icon icon-thumbs-up"></span>
         <span className="photo-card-descriptions__likes">{props.photo.likes}</span>
